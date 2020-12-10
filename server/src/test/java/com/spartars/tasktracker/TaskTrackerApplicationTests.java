d51f1374c8ff791f2c1f44c717c235d558810c23
spartars <spartars@yandex.ru>
2020-12-10 16:58:07 +0300

    Added server side

---
+++ server/src/test/java/com/spartars/tasktracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartars.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
